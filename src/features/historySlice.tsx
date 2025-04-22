156833092a54b31f417a4a3f8cf2c8e94a8d1dc6
Inryoku <dadadaiki0730@gmail.com>
2025-04-22 21:53:01 +0900

    add some files

---
+++ src/features/historySlice.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function historySlice() {
+  return <div>historySlice</div>;
+}
---
+++ src/features/inputSlice.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function inputSlice() {
+  return <div>inputSlice</div>;
+}
